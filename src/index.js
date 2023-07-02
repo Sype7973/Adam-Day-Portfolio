6fcb38a707333a422b30229480c4f9425911f681
unknown <adam.day7973@gmail.com>
2023-07-02 14:03:53 +1000

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
-ReactDOM.render(
-  <React.StrictMode>
+const Root = () => (
+  <ChakraProvider>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-
+  </ChakraProvider>
 );
 
+export default Root;
 
