836e6aef1cdbd1fda4339c0b13ad9cf7205197cc
unknown <adam.day7973@gmail.com>
2023-07-02 14:06:03 +1000

    updated portfolio page

--- src/components/Porfolio.js
+++ src/components/Porfolio.js
@@ -1,11 +1,38 @@
-import react from 'react';
+import React from 'react';
+import { Box, Flex, Grid, GridItem, Image, Link } from '@chakra-ui/react';
 
 export default function Portfolio() {
-    return (
-        <div>
-            <h1>Portfolio</h1>
-        </div>
-    )
-    // displays cards of projects that are links to the deployed app and the github repo
-    
+  const projects = [
+    {
+      title: 'Project 1',
+      imageSrc: 'https://example.com/project1.jpg',
+      deployedLink: 'https://example.com/project1',
+      githubLink: 'https://github.com/user/project1',
+    },
+    // Add more projects here...
+  ];
+
+  return (
+    <Box py={8}>
+      <h1>Portfolio</h1>
+      <Grid templateColumns="repeat(auto-fit, minmax(200px, 1fr))" gap={6} mt={8}>
+        {projects.map((project, index) => (
+          <GridItem key={index}>
+            <Link href={project.deployedLink} target="_blank" rel="noopener noreferrer">
+              <Image src={project.imageSrc} alt={project.title} />
+            </Link>
+            <Box mt={2} textAlign="center">
+              <Link href={project.deployedLink} target="_blank" rel="noopener noreferrer">
+                Deployed App
+              </Link>
+              <br />
+              <Link href={project.githubLink} target="_blank" rel="noopener noreferrer">
+                GitHub Repo
+              </Link>
+            </Box>
+          </GridItem>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
