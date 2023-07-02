e9728f07c453055fd0474c7aa2abf5d33ee80a3c
unknown <adam.day7973@gmail.com>
2023-07-02 13:25:23 +1000

    updated contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,59 @@
-import react from 'react';
+import React, { useState } from 'react';
 
-// email form with message
 export default function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-    return (
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form submission logic here
+    // can access the form values via the 'name', 'email', and 'message' variables
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
+
+    // Reset the form fields
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  return (
+    <div>
+      <h1>Contact</h1>
+      <form onSubmit={handleSubmit}>
         <div>
-            <h1>Contact</h1>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
         </div>
-    )
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
