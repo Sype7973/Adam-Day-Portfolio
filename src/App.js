2ae8982c7ad6dd56e81ee0da7fc1f6c339e8b700
unknown <adam.day7973@gmail.com>
2023-07-02 13:59:13 +1000

    updated code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // This is the main file for the portfolio website. It is the first file that is run when the website is loaded.
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // import the routes
-import Navbar from './Navbar';
-import AboutMe from './AboutMe';
-import Resume from './Resume';
-import Projects from './Projects';
-import Contact from './Contact';
-import Footer from './Footer';
+import Navbar from './components/Navbar';
+import AboutMe from './components/aboutMe';
+import Resume from './components/Resume';
+import Portfolio from './components/Porfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -18,13 +18,13 @@ function App() {
         <Router>
           <Navbar />
 
-          <Switch>
+          <Routes>
             <Route exact path="/" component={AboutMe} />
             <Route path="/about" component={AboutMe} />
             <Route path="/resume" component={Resume} />
-            <Route path="/portfolio" component={Projects} />
+            <Route path="/portfolio" component={Portfolio} />
             <Route path="/contact" component={Contact} />
-          </Switch>
+        </Routes>
 
           <Footer />
         </Router>
